28fe88dbbbf855c445e687686880a7013e57695e
Ryan Carrier <ryan.j.carrier@gmail.com>
2017-03-17 09:58:47 +1000

    gitignore and begginning

---
+++ constants.go
@@ -0,0 +1,4 @@
+package independentreserveclient
+
+//Domain is the default domain for the independentreserve api
+const Domain = "https://api.independentreserve.com"
--- serverSpecifics.go
+++ serverSpecifics.go
@@ -1,7 +1,22 @@
-package independentreservego
+package independentreserveclient
 
+import "encoding/json"
+
+/*
 GetPrimaryCurrencies() ([]string, error)
 GetSecondaryCurrencies() ([]string, error)
 GetTransactionCost(CurrencyFrom, CurrencyTo string) (cryptoclientgo.Cost, error)
 GetWithdrawCost(Currency string) (cryptoclientgo.Cost, error)
 GetDepositCost(Currency string) (cryptoclientgo.Cost, error)
+*/
+
+//GetPrimaryCurrencies gets the primary currencies available
+func GetPrimaryCurrencies() ([]string, error) {
+	got, err := getAndRead(Domain + "/Public/GetValidPrimaryCurrencyCodes")
+	if err != nil {
+		return []string{}, err
+	}
+	var l []string
+	err = json.Unmarshal(got, l)
+	return l, err
+}
